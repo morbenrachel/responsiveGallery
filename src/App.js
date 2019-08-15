bab47c1ff79e77f3512b73f74ade69926819066e
morbenrachel <morbenra@gmail.com>
2019-08-15 16:19:08 +0300

    no cachedImageList in localStorage

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import React from "react";
-import SearchBar from "./components/SearchBar";
-import SearchTerms from "./components/SearchTerms";
+import SearchInput from "./components/SearchInput";
 import GridGallery from "./components/GridGallery";
 import DropDownList from "./components/DropDownList";
-import "./App.css";
-import { checkHttpStatus, parseJSON } from "./flickrUtils";
+import { fetchImagesAsync } from "./flickrUtils";
 import _ from "lodash";
-import { Dropdown, DropdownButton } from "react-bootstrap";
 import constants from "./constants";
+import "./App.css";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    localStorage.removeItem(constants.LOCAL_STORAGE_KEY);
-    this.onSearch = _.debounce(this.onSearch, constants.DEBOUNCE_VALUE); //perhaps debounce onSearch instead of fetchImages
+    localStorage.removeItem(constants.LOCAL_STORAGE_KEY); //********** */
+    this.onSearch = _.debounce(this.onSearch, constants.DEBOUNCE_VALUE);
     const savedSearches = JSON.parse(
       localStorage.getItem(constants.LOCAL_STORAGE_KEY)
     );
     this.state = {
+      noResult: false,
       searchValue: "",
-      selectedSearchTerm: null,
       images: [],
       previousSearches: savedSearches ? savedSearches : []
     };
   }
 
-  //we want to save searches that were actually performed - after debounce timeout passed
-  //verify correct way of settings state of an array (pure - don't mutate)
-  //todo add logic for THRESHOLD of prev searches to display
-  updateLocalStorage(searchValue, cachedImages) {
+  updateLocalStorage(searchValue, cachedImagesList) {
     let searchTerms = this.state.previousSearches;
     if (!searchTerms.includes(searchValue)) {
       console.log("search term len" + searchTerms.length);
@@ -40,6 +35,7 @@ export default class App extends React.Component {
       this.setState({
         previousSearches: [...searchTerms, searchValue]
       });
+      console.log(this.state.previousSearches.length);
       localStorage.setItem(
         constants.LOCAL_STORAGE_KEY,
         JSON.stringify(this.state.previousSearches)
@@ -47,57 +43,44 @@ export default class App extends React.Component {
     }
   }
 
-  fetchImages(searchValue) {
-    const url =
-      constants.FLICKR_BASE_URL +
-      "&text=" +
-      searchValue +
-      "&per_page=" +
-      constants.IMAGES_PER_PAGE;
-    fetch(url)
-      .then(checkHttpStatus)
-      .then(parseJSON)
-      .then(response => {
-        console.log(response);
-        console.log(response.photos.total);
-        if (response.photos.total > 0) {
-          this.updateLocalStorage(searchValue);
-          this.setState({ images: response.photos.photo });
-        } else {
-          this.setState({ images: [] });
-        }
-
-        //maybe this is the best place to update storage
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+  onSearch = searchValue => {
+    console.log("search field entered: " + searchValue);
+    fetchImagesAsync(searchValue).then(imagesArray => {
+      console.log("inside on search fetch async");
+      console.log(imagesArray.length === 0);
+      this.setState({ noResult: imagesArray.length === 0 });
+      this.setState({ images: imagesArray });
+      this.updateLocalStorage(searchValue, imagesArray);
+    });
+  };
 
   isEmptySearchTerm = searchValue => {
     return searchValue.trim().length === 0;
   };
 
-  handleInputChange = () => {};
-
-  onSearch = searchValue => {
-    console.log("search field entered: " + searchValue);
-    if (!this.isEmptySearchTerm(searchValue)) {
-      // this.setState({ searchValue }); //todo verify if this is needed
-      this.fetchImages(searchValue); //based on results - if empty show user a text that says no results for search term, only if there are results it's worth saving
-    }
+  setSearchValue = valueFromInput => {
+    console.log("set search val");
+    this.setState({ searchValue: valueFromInput });
   };
 
   componentWillUnmount() {
     this.onSearch.cancel();
   }
 
+  getPreviousSearchTerms = () => {
+    return _.map(this.state.previousSearches, searchPair => {
+      _.pick(searchPair, "searchValue");
+    });
+  };
+
   handleSearchTermClick = searchTerm => {
-    //todo set text in input field to searchTerm
     console.log("search term: " + searchTerm);
     if (this.state.searchValue !== searchTerm) {
       this.setState({ searchValue: searchTerm });
-      this.fetchImages(searchTerm);
+      fetchImagesAsync(searchTerm).then(imagesArray => {
+        this.setState({ noResult: imagesArray.length === 0 });
+        this.setState({ images: imagesArray });
+      });
     }
   };
 
@@ -106,69 +89,32 @@ export default class App extends React.Component {
       <div>
         <div className="top-container">
           <h2>Image Gallery</h2>
-          {/* <SearchBar
+          <SearchInput
+            value={this.state.searchValue}
+            setSearchValue={this.setSearchValue}
             onSearch={this.onSearch}
-            reset={this.state.resetInputField}
-            value={this.state.searchValue}
-          /> */}
-          <input
-            value={this.state.searchValue}
-            className="inputStyle"
-            type="text"
-            placeholder="Search for image"
-            onChange={e => {
-              this.setState({ searchValue: e.target.value });
-              this.onSearch(e.target.value);
-            }}
           />
           {this.state.previousSearches.length > 0 ? (
-            <div>
-              Previous Searches:
-              <select
-                onChange={e => this.handleSearchTermClick(e.target.value)}
-              >
-                {_.map(this.state.previousSearches, searchTerm => {
-                  return (
-                    <option value={searchTerm} key={searchTerm}>
-                      {searchTerm}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
+            <DropDownList
+              handleSearchTermClick={this.handleSearchTermClick}
+              previousSearches={this.state.previousSearches}
+              searchValue={this.state.searchValue}
+            />
           ) : (
             ""
           )}
-          {/* {this.state.previousSearches.length > 0 ? (
-            <DropdownButton
-              id="dropdown-basic-button"
-              title="Previous Searches"
-            >
-              {_.map(this.state.previousSearches, searchTerm => {
-                return (
-                  <Dropdown.Item
-                    href="#/action-1"
-                    key={searchTerm}
-                    onClick={() => this.handleSearchTermClick(searchTerm)}
-                  >
-                    {searchTerm}
-                  </Dropdown.Item>
-                );
-              })}
-            </DropdownButton>
-          ) : (
-            ""
-          )} */}
-          {/* {this.state.previousSearches.length > 0 ? (
-            <SearchTerms previousSearches={this.state.previousSearches} />
-          ) : (
-            ""
-          )} */}
         </div>
         {this.state.images.length > 0 ? (
           <GridGallery images={this.state.images} />
         ) : (
-          <div>No results for {this.state.searchValue} </div>
+          ""
+        )}
+        {this.state.noResult ? (
+          <div className="no-results">
+            No Results For: {this.state.searchValue}
+          </div>
+        ) : (
+          ""
         )}
       </div>
     );
--- src/flickrUtils.js
+++ src/flickrUtils.js
@@ -1,7 +1,9 @@
+import constants from "./constants";
+
 const BASE_URL =
   "https://api.flickr.com/services/rest/?method=flickr.photos.search&safe_search=1&format=json&nojsoncallback=1&api_key=bac9f1ccfd854f27894fd47c4f01b1e8&content_type=1&is_getty=1";
 
-export function checkHttpStatus(response) {
+function checkHttpStatus(response) {
   if (response.status >= 200 && response.status < 300) {
     return response;
   } else {
@@ -9,12 +11,33 @@ export function checkHttpStatus(response) {
   }
 }
 
-/* To parse response to JSON object */
-export function parseJSON(response) {
-  return response.json();
+function getImagesArray(response) {
+  console.log("inside getimagesarray");
+  if (response.photos.total > 0) {
+    return response.photos.photo;
+  } else {
+    return [];
+  }
 }
 
-/* Generate image url */
+export async function fetchImagesAsync(searchValue) {
+  const url =
+    constants.FLICKR_BASE_URL +
+    "&text=" +
+    searchValue +
+    "&per_page=" +
+    constants.IMAGES_PER_PAGE;
+  try {
+    const response = await fetch(url);
+    checkHttpStatus(response);
+    const data = await response.json();
+    let imagesArray = await getImagesArray(data);
+    return imagesArray;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 export function getImageUrl(farm, server, id, secret) {
   return `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`;
 }
